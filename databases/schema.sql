7f168292635c4dd4a5232d1a46c961777d5516c0
Amet <ametueraspirant@yahoo.com>
2020-01-25 11:31:38 -0700

    almost ready for heroku

--- databases/schema.sql
+++ databases/schema.sql
@@ -6,6 +6,7 @@ create table bergers
 	id int not null auto_increment,
 	name varchar(255) not null,
 	eaten boolean default false,
+    createdAt TIMESTAMP default CURRENT_TIMESTAMP,
 	primary key (id)
 );
 
--- public/js/logics.js
+++ public/js/logics.js
@@ -1,11 +1,43 @@
 $(function() {
-	var burgersave;
 	$("#order").on("click", function(event)
 	{
 		event.preventDefault();
-		burgersave = $("#bergertext").val();
+		let burgersave = 
+		{
+			name: $("#bergertext").val().trim(),
+			eaten: 0
+		};
 		$("#bergertext").val('');
 		console.log(burgersave);
+
+		$.ajax("/api/bergers", {
+    		type: "POST",
+    		data: burgersave
+    	})
+		.then(function()
+		{
+        	console.log("order up!");
+			location.reload();
+		});
+	});
+
+	$(".eat-it").on("click", function(event)
+	{
+		event.preventDefault();
+		let id = $(this).data("id");
+
+		let eatchange = {
+			eaten: 1
+		}
+
+		$.ajax("/api/bergers/" + id, {
+			type: "PUT",
+			data: eatchange
+		})
+		.then(function()
+		{
+			location.reload();
+		});
 	});
 });
 
